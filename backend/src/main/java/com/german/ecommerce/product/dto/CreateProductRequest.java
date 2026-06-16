cd2d8eb859cc12190a63cdd192e54941ea3a0ca6
Germán Mora <dmm71317@gmail.com>
2026-06-16 15:38:35 -0600

    feat(product): add request and response dto models

---
+++ backend/src/main/java/com/german/ecommerce/product/dto/CreateProductRequest.java
@@ -0,0 +1,42 @@
+package com.german.ecommerce.product.dto;
+
+import java.math.BigDecimal;
+
+public class CreateProductRequest {
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
