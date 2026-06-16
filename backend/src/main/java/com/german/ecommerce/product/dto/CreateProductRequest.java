2dac687aa7e0e7cb693ea1769d7818a36ebaf047
Germán Mora <dmm71317@gmail.com>
2026-06-16 15:57:05 -0600

    feat(product): Activate validations in product creation

--- backend/src/main/java/com/german/ecommerce/product/dto/CreateProductRequest.java
+++ backend/src/main/java/com/german/ecommerce/product/dto/CreateProductRequest.java
@@ -3,9 +3,21 @@ package com.german.ecommerce.product.dto;
 import java.math.BigDecimal;
 
 public class CreateProductRequest {
+
+    @NotBlank(message = "SKU is required")
+    @Size(max = 50, message = "SKU cannot exceed 50 characters")
     private String sku;
+
+    @NotBlank(message = "Product name is required")
+    @Size(max = 255, message = "Product name cannot exceed 255 characters")
     private String name;
+
+    @NotBlank(message = "Description is required")
+    @Size(max = 2000, message = "Description cannot exceed 2000 characters")
     private String description;
+
+    @NotNull(message ="Price is required")
+    @DecimalMin(value="0.01", message = "Price must be greater than zero")
     private BigDecimal price;
 
     public String getSku() {
