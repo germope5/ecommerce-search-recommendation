f228fbe4037bc55f3b6b7ebdc3dc403bc50bbec4
Germán Mora <dmm71317@gmail.com>
2026-06-18 14:36:47 -0600

    feat(product): add API response errors in the business logic

---
+++ backend/src/main/java/com/german/ecommerce/exception/ApiErrorResponse.java
@@ -0,0 +1,48 @@
+package com.german.ecommerce.exception;
+
+import java.util.Map;
+
+public class ApiErrorResponse {
+    private int status;
+    private String message;
+    private Map<String, String> errors;
+
+    public ApiErrorResponse(){}
+
+    //Errores de negocio,(404,409,etc)
+    public ApiErrorResponse(int status, String message){
+        this.status = status;
+        this.message = message;
+    }
+
+    //Errores de validación (400)
+    public ApiErrorResponse(int status, Map<String, String> errors){
+        this.status = status;
+        this.errors = errors;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+    public void setErrors(Map<String, String> errors) {
+        this.errors = errors;
+    }
+    
+
+}
