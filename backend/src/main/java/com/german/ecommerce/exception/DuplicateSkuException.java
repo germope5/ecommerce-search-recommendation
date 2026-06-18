db255d367cb4648ab499dadd37b1d804587f5c9b
Germán Mora <dmm71317@gmail.com>
2026-06-18 14:25:03 -0600

    fix(product): change parameters in constructor method

--- backend/src/main/java/com/german/ecommerce/exception/DuplicateSkuException.java
+++ backend/src/main/java/com/german/ecommerce/exception/DuplicateSkuException.java
@@ -2,7 +2,7 @@ package com.german.ecommerce.exception;
 
 public class DuplicateSkuException extends RuntimeException {
 
-    public DuplicateSkuException() {
-        super("SKU already exists");
+    public DuplicateSkuException(String message) {
+        super(message);
     }
 }
