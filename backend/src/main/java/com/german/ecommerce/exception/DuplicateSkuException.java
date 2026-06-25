7cef6b15407941b173a7bd366dc78ac39dca48a0
Germán Mora <dmm71317@gmail.com>
2026-06-25 13:23:59 -0600

    fixproduct): add empty constructor to Duplicate & ProductNotFound

--- backend/src/main/java/com/german/ecommerce/exception/DuplicateSkuException.java
+++ backend/src/main/java/com/german/ecommerce/exception/DuplicateSkuException.java
@@ -2,6 +2,9 @@ package com.german.ecommerce.exception;
 
 public class DuplicateSkuException extends RuntimeException {
 
+    //Constructor vacio
+    public DuplicateSkuException() {}
+    
     public DuplicateSkuException(String message) {
         super(message);
     }
--- backend/src/main/java/com/german/ecommerce/exception/ProductNotFoundException.java
+++ backend/src/main/java/com/german/ecommerce/exception/ProductNotFoundException.java
@@ -2,6 +2,10 @@ package com.german.ecommerce.exception;
 
 public class ProductNotFoundException extends RuntimeException {
 
+    //Constructor vacio
+    public ProductNotFoundException() {}
+
+
     public ProductNotFoundException(String message) {
         super(message);
     }
